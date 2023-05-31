a53fb80a2afddfba957f45a10bc90f73e22dd2d2
rohansp004 <113821669+rohansp004@users.noreply.github.com>
2023-05-31 12:52:17 +0530

    Update and rename IplDemo.java to Demo.java

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Practical{
+  public static void main(String[] rohan){
+    
+    int arr[] = new int[]{10,20,30,40};
+    
+    for(int i=0; i<arr.length; i++){
+      System.out.println("MY Array: "+ arr[i]);
+    }
+    
+  }
+}
